a2a9b1aed744b3018bf0ee10568d53b67fb4ca02
arthur85c <arthur85c@gmail.com>
2018-08-09 14:49:22 -0400

    Added a editable property
    - Added an editable property that is a boolean

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,3 +1,7 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :name, :houseno, :stname, :city, :zipcode, :area, :startdate, :enddate, :email, :price, :user_id
+  attributes :id, :name, :houseno, :stname, :city, :zipcode, :area, :startdate, :enddate, :email, :price, :user_id, :editable
+
+  def editable
+    scope == object.user
+  end
 end
